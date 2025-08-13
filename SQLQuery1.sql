ac3f52f42ae922c1a32114ee1ed10a361ca60e32
Varad Gavhad <130216330+Varad912@users.noreply.github.com>
2025-08-14 02:44:18 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,126 @@
+select count(*) from blinkit_data
+select * from blinkit_data
+
+update blinkit_data
+set Item_Fat_Content = 
+case
+when Item_Fat_Content in ('LF','low fat') then 'Low Fat'
+when Item_Fat_Content = 'reg' then 'Regular'
+else Item_Fat_Content
+end
+
+select distinct(Item_Fat_Content) from blinkit_data
+
+SELECT CAST(SUM(Total_Sales) / 1000000.0 AS DECIMAL(10,2)) AS Total_Sales_Million
+FROM blinkit_data;
+
+SELECT CAST(AVG(Total_Sales) AS decimal(10,1)) AS Avg_Sales
+FROM blinkit_data;
+
+SELECT COUNT(*) AS No_of_Items
+FROM blinkit_data;
+
+SELECT CAST(AVG(Rating) AS DECIMAL(10,1)) AS Avg_Rating
+FROM blinkit_data;
+
+SELECT Item_Fat_Content, CAST(SUM(Total_Sales)/1000 AS DECIMAL(10,2)) AS Total_Sales_Thousands,
+       CAST(AVG(Rating) AS DECIMAL(10,1)) AS Avg_Rating,
+	   COUNT(*) AS No_of_Items,
+	   CAST(AVG(Total_Sales) AS decimal(10,1)) AS Avg_Sales
+FROM blinkit_data
+GROUP BY Item_Fat_Content
+order by Total_Sales_Thousands desc
+
+
+SELECT Item_Type, CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales,
+       CAST(AVG(Rating) AS DECIMAL(10,1)) AS Avg_Rating,
+	   COUNT(*) AS No_of_Items,
+	   CAST(AVG(Total_Sales) AS decimal(10,1)) AS Avg_Sales
+FROM blinkit_data
+GROUP BY Item_Type
+order by Total_Sales desc
+
+
+SELECT Outlet_Location_Type,
+       Item_Fat_Content,
+       CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales,
+       CAST(AVG(Rating) AS DECIMAL(10,1)) AS Avg_Rating,
+	   COUNT(*) AS No_of_Items,
+	   CAST(AVG(Total_Sales) AS decimal(10,1)) AS Avg_Sales
+FROM blinkit_data
+GROUP BY Outlet_Location_Type, Item_Fat_Content
+order by Total_Sales asc
+
+
+SELECT Outlet_Location_Type, 
+       ISNULL([Low Fat], 0) AS Low_Fat, 
+       ISNULL([Regular], 0) AS Regular
+FROM 
+(
+    SELECT Outlet_Location_Type, Item_Fat_Content, 
+           CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales
+    FROM blinkit_data
+    GROUP BY Outlet_Location_Type, Item_Fat_Content
+) AS SourceTable
+PIVOT 
+(
+    SUM(Total_Sales) 
+    FOR Item_Fat_Content IN ([Low Fat], [Regular])
+) AS PivotTable
+ORDER BY Outlet_Location_Type;
+
+
+SELECT Outlet_Establishment_Year, CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales,
+                                  CAST(AVG(Rating) AS DECIMAL(10,1)) AS Avg_Rating,
+	                              COUNT(*) AS No_of_Items,
+	                              CAST(AVG(Total_Sales) AS decimal(10,1)) AS Avg_Sales
+FROM blinkit_data
+GROUP BY Outlet_Establishment_Year
+ORDER BY Total_Sales desc
+
+select * from blinkit_data
+
+
+SELECT 
+    Outlet_Size, 
+    CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales,
+    CAST((SUM(Total_Sales) * 100.0 / SUM(SUM(Total_Sales)) OVER()) AS DECIMAL(10,2)) AS Sales_Percentage
+FROM blinkit_data
+GROUP BY Outlet_Size
+ORDER BY Total_Sales DESC;
+
+
+SELECT Outlet_Location_Type, CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales,
+                             CAST((SUM(Total_Sales) * 100.0 / SUM(SUM(Total_Sales)) OVER()) AS DECIMAL(10,2)) AS Sales_Percentage,
+                             CAST(AVG(Rating) AS DECIMAL(10,1)) AS Avg_Rating,
+	                         COUNT(*) AS No_of_Items,
+	                         CAST(AVG(Total_Sales) AS decimal(10,1)) AS Avg_Sales
+FROM blinkit_data
+GROUP BY Outlet_Location_Type
+ORDER BY Total_Sales DESC
+
+
+SELECT Outlet_Type, 
+CAST(SUM(Total_Sales) AS DECIMAL(10,2)) AS Total_Sales,
+		CAST(AVG(Total_Sales) AS DECIMAL(10,0)) AS Avg_Sales,
+		COUNT(*) AS No_Of_Items,
+		CAST(AVG(Rating) AS DECIMAL(10,2)) AS Avg_Rating,
+		CAST(AVG(Item_Visibility) AS DECIMAL(10,2)) AS Item_Visibility
+FROM blinkit_data
+GROUP BY Outlet_Type
+ORDER BY Total_Sales DESC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
